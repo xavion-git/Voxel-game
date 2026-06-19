347ead1ae03659773a3c5c6e3c6778b0c6dce6b3
Xavion-git <xdean1@nait.ca>
2026-06-18 22:54:59 -0600

    Left face

--- block.cpp
+++ block.cpp
@@ -11,11 +11,24 @@ pRenderer-> TranslateWorldMatrix( blockPosition.x, blockPosition.y, blockPositio
 // Render the block using a simple cube model.
 pRenderer->SetRenderMode( RM_SOLID );
 pRenderer->EnableImmediateMode( IM_QUADS );
-
 pRenderer-> ImmediateColor( 1.0 f, 1.0 f, 1.0 f, 1.0 f);
-pRenderer-> ImmediateNormal( 0.0 f, 0.0 f, -1.0 f); 
 
+pRenderer-> ImmediateNormal( 0.0 f, 0.0 f, -1.0 f); 
 pRenderer-> ImmediateVertex(l_length, -l_height,-l_width); // Front face
 pRenderer-> ImmediateVertex(-l_length, -l_height,-l_width);
 pRenderer->ImmediateVertex(-l_length, l_height, -l_width);
 pRenderer->ImmediateVertex(l_length, l_height, -l_width);
+
+// Repeat for the other faces of the cube, changing the normal and vertex positions accordingly.
+pRenderer-> ImmediateNormal( 0.0 f, 0.0 f, 1.0 f);
+pRenderer->ImmediateVertex(-l_length, -l_height, l_width);
+pRenderer->ImmediateVertex(l_length, -l_height, l_width);
+pRenderer->ImmediateVertex(l_length, l_height, l_width);
+pRenderer->ImmediateVertex(-l_length, l_height, l_width);
+
+// Left face
+pRenderer->ImmediateNormal(1.0 f, 0.0 f, 0.0 f);
+pRenderer->ImmediateVertex(l_length, -l_height, l_width);
+pRenderer->ImmediateVertex(l_length, -l_height, -l_width);
+pRenderer->ImmediateVertex(l_length, l_height, -l_width);
+pRenderer->ImmediateVertex(l_length, l_height, l_width);
